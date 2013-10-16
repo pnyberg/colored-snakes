5638251ed3024f77ef486fcafd0162aff84b1024
pnyberg <per_skuru@msn.com>
2013-10-16 17:59:38 +0200

    Update KeyOption.java
    Comments fixed/added

--- src/KeyOption.java
+++ src/KeyOption.java
@@ -1,14 +1,18 @@
-
+/**
+ * A class to save the keys with the name and 
+ * the keycode for the key
+ * 
+ * @author Per Nyberg
+ * @version 2013.10.16
+ */
 public class KeyOption {
-	// value of the key
+	// value of the key (KeyEvent.[keyValue])
 	private int keyValue;
 	// the text representing the key
 	private String keyText;
 	
 	/**
-	 * Create a KeyOption-object
-	 * @param keyValue
-	 * @param keyText
+	 * Create a KeyOption-object with a 
 	 */
 	public KeyOption(int keyValue, String keyText) {
 		this.keyValue = keyValue;
@@ -28,10 +32,4 @@ public class KeyOption {
 	public int getValue() {
 		return keyValue;
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
